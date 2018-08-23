a816e37095acc58f1fbca8bada8575442559c751
Slushy <ess090502@gmail.com>
2018-08-22 23:06:39 -0500

    added kfolds data splitting and stemming and tokenizing

--- lyricSorter.py
+++ lyricSorter.py
@@ -5,6 +5,8 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.stem.porter import PorterStemmer
+from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import StratifiedKFold
 stemmer = PorterStemmer()
 analyzer = CountVectorizer().build_analyzer()
 #TODO stop word removal, stemming example here https://github.com/ganigavinaya/Music_classification/blob/master/Main.py, then EDA, and later SVM look up
@@ -12,6 +14,7 @@ stopWords = set(stopwords.words("english"))#get list of stopwords
 artistName = "Artist_Name"
 lyric = "Lyric"
 lyricsDataFrame = pd.read_table("lyrics.txt", names=[artistName, lyric])
+randState = 7
 print(lyricsDataFrame)
 print(lyricsDataFrame.isnull().values.any())#quick check for nulls
 print(lyricsDataFrame[artistName].unique())#make sure there are only 2 unique artists and no misspellings
@@ -25,8 +28,15 @@ valueCountDataFrame = pd.DataFrame(lyricsDataFrame[lyric].value_counts().reset_i
 print(valueCountDataFrame)#print repeats in the dataframe in another data frame
 #repeats not that effecting and stuff subbbed out correctly
 #possible need to do stemming first
-#TODO fix these
 lyricsDataFrame[lyric] = lyricsDataFrame[lyric].apply(lambda x: " ".join([stemmer.stem(word=word) for word in analyzer(x)]))
 print(lyricsDataFrame)#makes all words into their basic root such as fish from fishing
-lyricsDataFrame[lyric] = lyricsDataFrame[lyric].apply(lambda x: " ".join([w for w in word_tokenize(x) if not w in stopWords]))#TODO fix leaves contraction ends
+lyricsDataFrame[lyric] = lyricsDataFrame[lyric].apply(lambda x: " ".join([w for w in word_tokenize(x) if not w in stopWords]))
 print(lyricsDataFrame)#removes stopwords
+#train test splits
+kFolds = StratifiedKFold(n_splits=5, random_state=randState)#kfolds set up
+lyrics = lyricsDataFrame[lyric]
+artists = lyricsDataFrame[artistName]
+for train_index, test_index in kFolds.split(lyrics, artists):
+    x_train, x_test = lyrics[train_index], lyrics[test_index]
+    y_train, y_test = artists[train_index], lyrics[test_index]#split up with kfolds
+    #TODO build model and run and train it
